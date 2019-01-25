fbc49d81d509dd87ca6f64b5c559df72412bc254
William Fox <wfox96@gmail.com>
2019-01-25 13:34:15 -0800

    save before branch

--- a01/tokenizer.cpp
+++ a01/tokenizer.cpp
@@ -1,12 +1,42 @@
 //
 // Created by William Fox on 1/24/19.
 //
-
+#include <cstdio>
+#include <readline/readline.h>
+#include <string>
+#include <list>
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello, World!";
+struct node {
+    string token;
+    node *next;
+};
+
+void printLinkedList(list ){
+    cout << "[";
+    while(head){
+        cout << "{" << head->token << "}";
+        head = head->next;
+        if(head){
+            cout << ",";
+        }
+    }
+
+    cout << "]" << endl;
+}
+
+int main() {
+    string userInput = readline(">");
+    node *head = new node;
+    node *tail = head;
+    head->token = userInput[0];
+    for(int i = 1; i < userInput.length(); i++){
+//        tail->next = new node;
+//        tail->next->token = userInput[i];
+//        tail = tail->next;
+    }
+//    cout << head->next->token << endl;
+//    printLinkedList(head->next);
     return 0;
 }
