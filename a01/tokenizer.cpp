b397da990ede285fb63fe6002847c25fd5c3a47d
William Fox <wfox96@gmail.com>
2019-01-30 20:33:26 -0800

    cleaned up code for turn in

--- a01/tokenizer.cpp
+++ a01/tokenizer.cpp
@@ -2,7 +2,7 @@
 // Created by William Fox on 1/24/19.
 //
 #include <cstdio>
-#include <stdlib.h>
+#include <cstdlib>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <string>
@@ -13,51 +13,57 @@
 
 using namespace std;
 
-void printLinkedList(list <string> tokens);
+void printLinkedList(list<string> tokens);
 
-list <string> readInput(string userInput);
+//Returns a linked list of tokenized strings
+list<string> readInput(string userInput);
 
+//Returns true if c is one of |, ;, <, >, &
 bool isSpecialCharacter(char c);
 
+//Returns true if c is '\'
 bool isEscapeCharacter(char c);
 
+//Returns true if c is whitespace
 bool isSpace(char c);
 
+//Returns true if c is a double or single quote
 bool isQuote(char c);
 
 //adds non empty Token to end of the passed List
-void addToken(list <string> &tokens, ostringstream &token);
+void addToken(list<string> &tokens, ostringstream &token);
 
 //clears current ostringstream (ie the token)
 void clearToken(ostringstream &token);
 
-//A helper function to set the corresponding flag for a quote
+//A helper function to set the corresponding flags for a quote character
 void setQuoteFlags(char c, bool &isSingleQuote, bool &isDoubleQuote);
 
 int main() {
-    list <string> tokens;
+    list<string> tokens;
     char *input;
-    while((input = readline(">"))){
+    while ((input = readline(">"))) {
         add_history(input);
         tokens = readInput(string(input));
         printLinkedList(tokens);
         free(input);
     }
-    //To reset outer shell indents
+    //Print newline to reset outer shell
     cout << endl;
     return 0;
 }
 
-list <string> readInput(string userInput) {
+list<string> readInput(string userInput) {
     bool insideDoubleQuotes = false;
     bool insideSingleQuotes = false;
-    list <string> tokens;
+    list<string> tokens;
     ostringstream token;
 
     for (string::iterator iter = userInput.begin(); iter != userInput.end(); iter++) {
         if (isEscapeCharacter(*iter) && !insideSingleQuotes) {
             //Prevents running out of bounds
-            if(iter != --userInput.end()) {
+            if (iter != --userInput.end()) {
+                //Add next character regardless
                 token << *(++iter);
             }
         } else if (isQuote(*iter)) {
@@ -70,15 +76,17 @@ list <string> readInput(string userInput) {
                 //Ending a single quote section
             } else if (*iter == '\'' && insideSingleQuotes) {
                 insideSingleQuotes = false;
+                //Inside quotes, add quote char as literal
             } else {
-                token << *(iter);
+                token << *iter;
             }
         } else if (isSpecialCharacter(*iter) && !insideSingleQuotes && !insideDoubleQuotes) {
+            //End/Add old token
             addToken(tokens, token);
             clearToken(token);
-
+            //Insert special char
             token << *iter;
-
+            //End/Add special char token
             addToken(tokens, token);
             clearToken(token);
         } else if (isSpace(*iter) && !insideSingleQuotes && !insideDoubleQuotes) {
@@ -93,9 +101,9 @@ list <string> readInput(string userInput) {
     return tokens;
 }
 
-void printLinkedList(list <string> tokens) {
-    if (tokens.empty()){
-	    return;
+void printLinkedList(list<string> tokens) {
+    if (tokens.empty()) {
+        return;
     }
     cout << "[";
     for (list<string>::iterator it = tokens.begin(); it != tokens.end(); it++) {
@@ -131,8 +139,8 @@ bool isQuote(char c) {
     return c == '\"' || c == '\'';
 }
 
-void addToken(list <string> &tokens, ostringstream &token) {
-        //Don't add empty tokens
+void addToken(list<string> &tokens, ostringstream &token) {
+    //Don't add empty tokens
     if (!token.str().empty()) {
         tokens.push_back(token.str());
     }
