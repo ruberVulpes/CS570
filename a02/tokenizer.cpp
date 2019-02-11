f7cb19435c1d02fa970ae00c3320722bc7bd99a0
William Fox <wfox96@gmail.com>
2019-02-10 17:23:52 -0800

    cleaned exeCommand and added execvp calls

--- a02/tokenizer.cpp
+++ a02/tokenizer.cpp
@@ -125,28 +125,41 @@ void executeCommands(list<list<string> > cleanCommandList) {
     for (list<list<string> >::iterator iter = cleanCommandList.begin(); iter != cleanCommandList.end(); iter++) {
         commandTokenSize = iter->size();
         char *argv[commandTokenSize + 1];
-        for (list<string>::iterator commandIter = iter->begin(); commandIter != iter->end(); commandIter++) {
-            if (*commandIter == "pwd") {
-                char buff[FILENAME_MAX];
-                if (!getcwd(buff, FILENAME_MAX)) {
-                    cout << "Unable to obtain current directory" << endl;
-                } else {
-                    cout << buff << endl;
-                }
-                break;
-            } else if (*commandIter == "cd") {
-                if (commandTokenSize != 2) {
-                    cout << "Accepts exactly one argument" << endl;
-                } else {
-                    const char * directory = next(commandIter, 1)->c_str();
-                    if (chdir(directory) != 0) {
-                        cout << "Directory does not exist or is not accessible" << endl;
-                    }
-                    break;
-                }
+        list<string>::iterator commandIter = iter->begin();
+        if (*commandIter == "pwd") {
+            char buff[FILENAME_MAX];
+            if (!getcwd(buff, FILENAME_MAX)) {
+                cout << "Unable to obtain current directory" << endl;
             } else {
-                continue;
+                cout << buff << endl;
             }
+        } else if (*commandIter == "cd") {
+            if (commandTokenSize != 2) {
+                cout << "Accepts exactly one argument" << endl;
+            } else {
+                const char * directory = next(commandIter, 1)->c_str();
+                if (chdir(directory) != 0) {
+                    cout << "Directory does not exist or is not accessible" << endl;
+                }
+            }
+        } else {
+            for (int i = 0; i < commandTokenSize; i++) {
+                char * castHelper = new char [commandIter->length() + 1];
+                strcpy(castHelper, commandIter->c_str());
+                argv[i] = castHelper;
+                commandIter++;
+            }
+            argv[commandTokenSize] = NULL;
+            int childPid = fork();
+            if(childPid == 0){
+                if(execvp(argv[0], argv))
+                    cout << "Unable to execute " << argv[0] << endl;
+            } else if (childPid > 0) {
+                wait(&childPid);
+            } else {
+                cout << "Unable to spawn program" << endl;
+            }
+
         }
     }
 }
