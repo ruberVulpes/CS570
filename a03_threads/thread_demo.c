8e4635ba406722260630b630fbe3efe39d6f05a4
William Fox <wfox96@gmail.com>
2019-03-05 15:58:00 -0800

    changed to join in a second for loop

--- a03_threads/thread_demo.c
+++ a03_threads/thread_demo.c
@@ -10,8 +10,10 @@ int main() {
     pthread_t threads[4];
     int thread_returns[4];
     int i;
-    for (i = 0; i < 5; ++i) {
+    for (i = 0; i < 5; i++) {
         thread_returns[i] = pthread_create(&threads[i], NULL, (void *)&worker, (void *) &i);
+    }
+    for(i = 0; i < 5; i++){
         pthread_join(threads[i], NULL);
     }
     printf("work completed.\n");
