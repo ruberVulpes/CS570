a5249e992b76016cef596abe58be0ff124bdfe20
William Fox <wfox96@gmail.com>
2019-03-05 16:25:10 -0800

    fixed threads sharing pointer error

--- a03_threads/thread_demo.c
+++ a03_threads/thread_demo.c
@@ -8,12 +8,13 @@
 
 int main() {
     pthread_t threads[5];
+    int values[] = {0, 1, 2, 3, 4};
     int thread_returns[5];
     int i;
-    for (i = 0; i < 5; ++i) {
-        thread_returns[i] = pthread_create(&threads[i], NULL, (void *)&worker, (void *) &i);
+    for (i = 0; i < 5; i++) {
+        thread_returns[i] = pthread_create(&threads[i], NULL, (void *)&worker, (void *) &values[i]);
     }
-    for(i = 0; i < 5; ++i){
+    for(i = 0; i < 5; i++){
         pthread_join(threads[i], (void *) &thread_returns[i]);
 
     }
