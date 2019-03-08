4822b7b74af054ba9d484d22a4873deb711a2e1b
William Fox <wfox96@gmail.com>
2019-03-08 15:27:25 -0800

    added working cmd line arg handler

--- a03/driver.cpp
+++ a03/driver.cpp
@@ -11,23 +11,34 @@
 
 using namespace std;
 int main(int argc, char *argv[]){
+    int argumentCount = 1;
     int nFlag = -1;
-    string fileName = "";
+    string inputFileName;
+    string outputFileName;
     int tFlag = 0;
     int option = 0;
-    while((option = getopt(argc, argv, "nft")) != -1){
-        switch (option){
-            case 'n' : nFlag = atoi(optarg);
+    while ((option = getopt(argc, argv, "n:f:t")) != -1) {
+        switch (option) {
+            case 'n' :
+                nFlag = atoi(optarg);
+                argumentCount += 2;
                 break;
-            case 'f' : fileName = string(optarg);
+            case 'f' :
+                outputFileName = string(optarg);
+                argumentCount += 2;
                 break;
-            case 't' : tFlag = 1;
+            case 't' :
+                tFlag = 1;
+                argumentCount++;
                 break;
             default:
-                cout << "idk" << endl;
+                break;
         }
     }
-    cout << nFlag << " file:" << fileName << " tflag: " << tFlag << " " << endl;
+    inputFileName = argv[argumentCount++];
+    int pageTableSizes[argc - argumentCount];
+    for (int i = 0; argumentCount < argc; i++) {
+        pageTableSizes[i] = atoi(argv[argumentCount++]);
+    }
     return 0;
-
 }
